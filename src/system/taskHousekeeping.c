616cb94b17116f1840f65538b462ddd118101f4a
Carlos Gonzalez C <contacto.carlosgonzalez@gmail.com>
2018-04-30 12:13:30 -0300

    Adding mission MOCKUP

--- src/system/taskHousekeeping.c
+++ src/system/taskHousekeeping.c
@@ -22,62 +22,138 @@
 
 static const char *tag = "Housekeeping";
 
+enum phase_n{
+    phase_a,
+    phase_a1,
+    phase_b,
+    phase_c,
+    phase_d
+};
+
 void taskHousekeeping(void *param)
 {
     LOGD(tag, "Started");
 
-    portTick delay_ms    = 1000;            //Task period in [ms]
+    portTick delay_ms    = 1000;        //Task period in [ms]
 
     unsigned int elapsed_sec = 0;       // Seconds counter
     unsigned int _10sec_check = 10;     // 10[s] condition
-    unsigned int _10min_check = 10*60;  // 10[m] condition
-    unsigned int _1hour_check = 60*60;  // 01[h] condition
+    unsigned int _01min_check = 60;     // 01[m] condition
+    unsigned int _05min_check = 5*60;   // 05[m] condition
+
+
+    int phase = 0;      // current phase
+    int balloons = 2;   // balloons available
 
     portTick xLastWakeTime = osTaskGetTickCount();
     
     while(1)
     {
 
-        osTaskDelayUntil(&xLastWakeTime, delay_ms); //Suspend task
-        elapsed_sec += delay_ms/1000; //Update seconds counts
+        osTaskDelayUntil(&xLastWakeTime, delay_ms); // Suspend task
+        elapsed_sec ++; //= delay_ms/1000; // Update seconds counts
 
-        // 1 seconds action
-//        cmd_t *cmd_dbg = cmd_get_str("debug_obc");
-//        cmd_add_params_var(cmd_dbg, 0);
-//        cmd_send(cmd_dbg);
+        phase = dat_get_system_var(dat_balloon_phase); // Determine current phase
 
-        // 2 seconds actions
-//        if((elapsed_sec % 2) == 0)
-//        {
-//            cmd_t *cmd_2s = cmd_get_str("sample_obc_sensors");
-//            cmd_send(cmd_2s);
-//        }
+        /**
+         * Phases A(0), A1(1), B(2) and C(3): sample radiosonde data at 1Hz and
+         * send beacon at 1/4 Hz
+         */
+        if(phase < phase_c)
+        {
+            //1 second action: sample radiosonde data
+            cmd_t *cmd_sonde_1 = cmd_get_str("get_weather");
+            cmd_send(cmd_sonde_1);
 
-        /* 10 seconds actions */
-//        if((elapsed_sec % _10sec_check) == 0)
-//        {
-//            LOGD(tag, "10 sec tasks");
-//            //cmd_t *cmd_10s = cmd_get_str("get_mem");
-//            cmd_t *cmd_10s = cmd_get_str("test");
-//            cmd_add_params_var(cmd_10s, "Task housekeeping running");
-//            cmd_send(cmd_10s);
-//        }
+            // 4 seconds actions: download radiosonde data
+            if((elapsed_sec % 2) == 0)
+            {
+                cmd_t *cmd_send_sonde_trx = cmd_get_str("send_weather_data");
+                cmd_send(cmd_send_sonde_trx);
+            }
+        }
 
-        /* 10 minutes actions */
-//        if((elapsed_sec % _10min_check) == 0)
-//        {
-//            LOGD(tag, "10 min tasks");
-//            cmd_t *cmd_10m = cmd_get_str("get_mem");
-//            cmd_add_params_var(cmd_10m, 0);
-//            cmd_send(cmd_10m);
-//        }
+        /**
+         * Phase A1 y B: execute star tracker + sample and send radiosonde data
+         * (see code above)
+         */
+        if(phase == phase_a1 || phase == phase_b)
+        {
+            if((elapsed_sec % _05min_check) == 0)
+            {
+                cmd_t *cmd_startracker = cmd_get_str("cmd_startracker");
+                cmd_send(cmd_startracker);
+            }
+        }
 
-        /* 1 hours actions */
-        if((elapsed_sec % _1hour_check) == 0)
+        /**
+         * Phase C: cut balloon, sample radiosonde data at 1Hz, send radiosonde
+         * data via Iridium at 1/10Hz, execute super-resolution experiment
+         * every minute.
+         */
+        if(phase == phase_c)
+        {
+            /* Send command to cut the one balloon */
+            if(balloons > 1)
+            {
+                balloons --;
+                cmd_t *cmd_cut_balloon = cmd_get_str("cut_balloon");
+                cmd_add_params_var(cmd_cut_balloon, 1); /* Cut first balloon */
+                cmd_send(cmd_cut_balloon);
+            }
+
+            /* Sample radiosonde data at 1HZ */
+            cmd_t *cmd_sonde_2 = cmd_get_str("get_weather");
+            cmd_send(cmd_sonde_2);
+
+            /* Send radiosonde data via Iridium every 10 seconds */
+            if((elapsed_sec % _10sec_check) == 0)
+            {
+                cmd_t *cmd_iridium = cmd_get_str("send_iridium");
+                cmd_send(cmd_iridium);
+            }
+
+            /* Execute super-resolution experiment every minute */
+            if((elapsed_sec % _01min_check) == 0)
+            {
+                cmd_t *cmd_resolution = cmd_get_str("superresolution");
+                cmd_send(cmd_resolution);
+            }
+        }
+
+        /**
+         * Phase D: cut the second balloon, keep sending radiosonde data via
+         * Iridium
+         */
+        if(phase == phase_d)
+        {
+            /* Send command to cut the second balloon */
+            if(balloons > 0)
+            {
+                balloons --;
+                cmd_t *cmd_cut_balloon = cmd_get_str("cut_balloon");
+                cmd_add_params_var(cmd_cut_balloon, 2); /* Cut second balloon */
+                cmd_send(cmd_cut_balloon);
+            }
+
+            /* Do not sample radiosonde data, but send existing radiosonde data
+             * via Iridium every 10 seconds */
+            if((elapsed_sec % _10sec_check) == 0)
+            {
+                cmd_t *cmd_iridium = cmd_get_str("send_iridium");
+                cmd_send(cmd_iridium);
+            }
+        }
+
+        /**
+         * Always to do list
+         */
+        /* 1 min actions, update minutes alive counter*/
+        if((elapsed_sec % _01min_check) == 0)
         {
             LOGD(tag, "1 hour check");
             cmd_t *cmd_1h = cmd_get_str("update_hours_alive");
-            cmd_add_params_var(cmd_1h, 1); // Add 1hr
+            cmd_add_params_var(cmd_1h, 1); // Add 1 min
             cmd_send(cmd_1h);
         }
     }
