fa10952a8fb3b0d2e66a3035ccbf681de8bdf4d7
gdiazh <g.hernan.diaz@gmail.com>
2018-11-02 01:48:40 -0300

    update launch dependencies

--- sandbox/launch/launch_aprs.sh
+++ sandbox/launch/launch_aprs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SUBMODULES=~/Spel/submodules/sw/bus-arquitecture
+SUBMODULES=/home/pi/Spel/sub_modules/
 cd $SUBMODULES
-direwolf -t 0 -c aprs/aprs_telem.conf  > ~/Spel/aprs.log 2>&1
-cd  ~/Spel
+direwolf -t 0 -c aprs/aprs_telem.conf  > ~/Spel/mission-history/aprs-"$(date "+%FT%T")".log 2>&1
+# cd  ~/Spel
