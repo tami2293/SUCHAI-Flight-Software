9d871fe2779a1dbc953f4d0907038c8e564aa7f4
root <matias.vidal.v@gmail.com>
2018-11-08 19:41:04 +0000

    Changed the installation file name from direwolf to aprs. Deleted the libgps installation, since it does not belong here.

--- sandbox/install/install_aprs.sh
+++ sandbox/install/install_aprs.sh
@@ -1,7 +1,6 @@
 #install direwolf
 #install subdep sound:
-#sudo apt-get install libasound2-dev
-#sudo apt-get install libgps-dev
+sudo apt-get install libasound2-dev
 cd ~
 git clone https://github.com/wb2osz/direwolf.git
 cd direwolf
